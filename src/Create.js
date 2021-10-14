8afd51c4211e9bd392c78df8a7ad182832b90c3a
Yusuf <yusufmohd72@gmail.com>
2021-10-14 20:49:51 +0530

    Add new blog

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
+    setIsPending(true);
+
     const blog = { title, body, author };
 
-    console.log(blog);
+    fetch(`http://localhost:8000/blogs`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    });
   };
 
   return (
@@ -36,7 +46,8 @@ const Create = () => {
           <option value="mario">Mario</option>
           <option value="yusuf">Yusuf</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
