e105c0fda713c25ee336d2c5125b4b1eacb1d49c
Yusuf <yusufmohd72@gmail.com>
2021-10-15 23:01:06 +0530

    Delete request

--- src/blogDetails.js
+++ src/blogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
@@ -9,6 +9,16 @@ const BlogDetails = () => {
     error,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
 
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE',
+    }).then(() => {
+      history.push('/');
+    });
+  };
+
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -18,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
